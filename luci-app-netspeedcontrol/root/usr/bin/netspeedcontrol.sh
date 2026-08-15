ebc67b95806f5e7319123387efdbe0034dbc99e3
xiaojianzheng <1272209235@qq.com>
2026-08-15 10:37:06 +0800

    fix(unit): 修正 8M 兆比特到字节速率 (Mbps -> MB/s / KB/s) 的精准换算逻辑，升级版本至 0.2.3-25

--- luci-app-netspeedcontrol/root/usr/bin/netspeedcontrol.sh
+++ luci-app-netspeedcontrol/root/usr/bin/netspeedcontrol.sh
@@ -178,9 +178,19 @@ rate_to_nft_bytes() {
 	fi
 
 	if echo "$raw_rate" | grep -qi "m"; then
-		echo "${rate} mbytes/second"
+		# 兆比特(Mbps) 转 字节速率(MB/s 或 KB/s): 1 Byte = 8 Bits
+		local mbytes=$(( rate / 8 ))
+		if [ "$mbytes" -ge 1 ]; then
+			echo "${mbytes} mbytes/second"
+		else
+			local kbytes=$(( rate * 1024 / 8 ))
+			echo "${kbytes} kbytes/second"
+		fi
 	else
-		echo "${rate} kbytes/second"
+		# 千比特(kbit/s) 转 字节速率(kbytes/second)
+		local kbytes=$(( (rate + 7) / 8 ))
+		[ "$kbytes" -lt 1 ] && kbytes=1
+		echo "${kbytes} kbytes/second"
 	fi
 }
 
